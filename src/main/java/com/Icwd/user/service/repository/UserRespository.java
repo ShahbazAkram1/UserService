fe46bd92246f641b4e632ce72b28bdf3b315e878
Shahbaz Khan <Jamal.Khan@crown.com>
2022-11-23 13:20:03 +0500

    added user rest api

--- src/main/java/com/Icwd/user/service/repository/UserRespository.java
+++
@@ -1,7 +0,0 @@
-package com.Icwd.user.service.repository;
-
-import com.Icwd.user.service.entity.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRespository extends JpaRepository<User, String> {
-}
